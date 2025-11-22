b8909eb5d916afe697c46b53bdeb5f288a447229
unknown <clne@alumnatflorida.es>
2025-11-22 17:48:28 +0100

    Añadidos estilos básicos

--- index.php
+++ index.php
@@ -55,14 +55,17 @@ if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
 <head>
     <meta charset="UTF-8">
     <title>Ahorcado</title>
+    <link rel="stylesheet" href="common.css">
 </head>
 <body>
     <h1>Juego del Ahorcado</h1>
     <p>Palabra secreta: <?php echo $_SESSION['letras_acertadas']; ?></p>
     <p>Vidas restantes: <?php echo $_SESSION['vidas']; ?></p>
     <form method="post">
-        <label for="letra">Introduce una letra:</label>
-        <input type="text" name="letra" id="letra" maxlength="1" required>
+        <label for="letra">
+          <span>Introduce una letra:</span>
+        <input type="text" name="letra" id="letra" maxlength="1" required placeholder="Letra">
+        </label>
         <button type="submit">Adivinar</button>
     </form>
     <p>Letras usadas: <?php echo implode(', ', $_SESSION['letras_usadas']); ?></p>
